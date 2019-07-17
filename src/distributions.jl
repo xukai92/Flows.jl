4156a55bfebbfcd1cda2731d837fd1d51be351d8
Kai Xu <xukai921110@gmail.com>
2019-07-17 09:40:10 +0100

    make MvNormal01 cpu and gpu compatible

--- src/distributions.jl
+++ src/distributions.jl
@@ -1,9 +1,10 @@
 # Define a MvNormal distribution that is easy to work with GPU
-struct MvNormal01
+struct MvNormal01{T}
     dim::Int
 end
+MvNormal01(dim::Int) = MvNormal01{:cpu}(dim)
 
 # The constant below is a hack to make things work on GPU.
 const LOG2PI32 = log(2Float32(pi))
 logpdf(d::MvNormal01, x) = sum(-(LOG2PI32 .+ x .* x) ./ 2; dims=1)
-rand(d::MvNormal01, n::Int=1) = randn(Float32, d.dim, n) |> gpu
+rand(d::MvNormal01{:cpu}, n::Int=1) = randn(Float32, d.dim, n)
--- src/gpu.jl
+++ src/gpu.jl
@@ -1,3 +1,10 @@
 using CuArrays: @cufunc
 
 @cufunc logit(x) = log(x) - log(1 - x)
+
+import Flux: cpu, gpu
+
+gpu(d::MvNormal01) = MvNormal01{:gpu}(d.dim)
+cpu(d::MvNormal01) = MvNormal01{:cpu}(d.dim)
+
+rand(d::MvNormal01{:gpu}, n::Int=1) = randn(Float32, d.dim, n) |> Flux.gpu
