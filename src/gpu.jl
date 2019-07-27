2181170895820dbf506d6fe8eeb7241442073612
Kai Xu <xukai921110@gmail.com>
2019-07-27 10:01:35 +0100

    fix CuArray's .^ bug

--- src/gpu.jl
+++ src/gpu.jl
@@ -1,4 +1,5 @@
 using CuArrays: @cufunc
+import CuArrays: culiteral_pow
 
 @cufunc logit(x) = log(x) - log(1 - x)
 @cufunc logistic(x) = inv(exp(-x) + 1)
@@ -10,3 +11,11 @@ gpu(d::MvNormal01) = MvNormal01{:gpu}(d.dim)
 cpu(d::MvNormal01) = MvNormal01{:cpu}(d.dim)
 
 rand(d::MvNormal01{:gpu}, n::Int=1) = randn(Float32, d.dim, n) |> Flux.gpu
+
+
+# Fix for broadcast ^
+culiteral_pow(::typeof(^), x::T, ::Val{0}) where {T<:Real} = one(x)
+culiteral_pow(::typeof(^), x::T, ::Val{1}) where {T<:Real} = x
+culiteral_pow(::typeof(^), x::T, ::Val{2}) where {T<:Real} = x * x
+culiteral_pow(::typeof(^), x::T, ::Val{3}) where {T<:Real} = x * x * x
+culiteral_pow(::typeof(^), x::T, ::Val{p}) where {T<:Real,p} = CUDAnative.pow(x, Int32(p))
